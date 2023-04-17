846fc73ffbce65cd2676df35b365fb268ce2cddd
scsk-nakagome <nakagome@scsk.jp>
2023-04-17 20:03:26 +0900

    4/17

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,66 +9,18 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type Employee struct {
-	Employeeid string `json:"employeeid"`
-	Firstname  string `json:"firstname"`
-	Lastname   string `json:"lastname"`
-	Divisionid string `json:"divisionid"`
-}
-
-var employees []Employee
-
-func getAllEmployees(w http.ResponseWriter, r *http.Request) {
-	employees = append(employees, Employee{Employeeid: "1", Firstname: "John", Lastname: "Doe", Divisionid: "1"})
-	employees = append(employees, Employee{Employeeid: "2", Firstname: "Smith", Lastname: "Taylor", Divisionid: "2"})
-	fmt.Println("Endopoint Hit: All Employees Endpoint")
-	json.NewEncoder(w).Encode(employees)
-}
-
 func main() {
 
 	log.Printf("starting HTTP")
 
 	r := gin.Default()
 
-	// r.Use(cors.New(cors.Config{
-	// 	AllowOrigins: []string{
-	// 		"http://localhost:3000",
-	// 		"http://172.20.10.3:3000",
-	// 		"https://localhost:3000",
-	// 		"https://172.20.10.3:3000",
-	// 	},
-	// 	AllowMethods: []string{
-	// 		"POST",
-	// 		"GET",
-	// 		"OPTIONS",
-	// 	},
-	// 	AllowHeaders: []string{
-	// 		"Access-Control-Allow-Headers",
-	// 		"Origin",
-	// 		"X-Requested-With",
-	// 		"Authorization",
-	// 		"Accept",
-	// 		"Accept-Encoding",
-	// 		"Access-Control-Allow-Credentials",
-	// 	},
-	// 	AllowCredentials: true,
-	// 	MaxAge:           24 * time.Hour,
-	// }))
-
-	// r.OPTIONS("/", func(c *gin.Context) {
-	// 	c.Header("Access-Control-Expose-Headers", "*")
-	// 	c.Header("Access-Control-Allow-Origin", "*")
-	// 	c.Header("Access-Control-Allow-Credentials", "true")
-	// 	c.JSON(204, gin.H{
-	// 		"message":              "hello I'm Cloud Run API 1!",
-	// 		"authorizationHeaders": c.GetHeader("Authorization"),
-	// 	})
-	// })
-
 	r.GET("/", func(c *gin.Context) {
-		// c.Header("Access-Control-Expose-Headers", "*")
-		// c.Header("Access-Control-Allow-Origin", "*")
+		// context から取得した authToken を受け取る
+		auth0Token, ok := c.Value("auth0-token").(string)
+		if !ok {
+			// エラー処理
+		}
 		requestDump, err := httputil.DumpRequest(c.Request, true)
 		if err != nil {
 			fmt.Println(err)
@@ -77,6 +28,7 @@ func main() {
 		c.JSON(http.StatusOK, gin.H{
 			"message":      "hello I'm Cloud Run API 1!",
 			"requstHeader": string(requestDump),
+			"auth0-token":  auth0Token,
 		})
 	})
 
@@ -84,28 +36,4 @@ func main() {
 		log.Fatal(err)
 	}
 
-	// コメントアウト
-	// http.HandleFunc("/", handler)
-	// http.HandleFunc("/allEmployees", getAllEmployees)
-
-	// // Determine port for HTTP service.
-	// port := os.Getenv("PORT")
-	// if port == "" {
-	// 	port = "8080"
-	// 	log.Printf("defaulting to port %s", port)
-	// }
-
-	// // Start HTTP server.
-	// log.Printf("listening on port %s", port)
-	// if err := http.ListenAndServe(":"+port, nil); err != nil {
-	// 	log.Fatal(err)
-	// }
 }
-
-// func handler(w http.ResponseWriter, r *http.Request) {
-// 	name := os.Getenv("NAME")
-// 	if name == "" {
-// 		name = "Cloud Run 1"
-// 	}
-// 	fmt.Fprintf(w, "Hello %s!\n", name)
-// }
