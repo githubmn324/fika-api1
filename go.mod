f6fe704fd0bd9798c4ebdb97310327135e41bac9
scsk-nakagome <nakagome@scsk.jp>
2023-04-17 19:46:58 +0900

    4/17

--- go.mod
+++ go.mod
@@ -3,11 +3,15 @@ module github.com/GoogleCloudPlatform/golang-samples/run/helloworld
 go 1.13
 
 require (
+	cloud.google.com/go/compute v1.19.1 // indirect
+	cloud.google.com/go/workflows v1.10.0 // indirect
 	github.com/bytedance/sonic v1.8.7 // indirect
 	github.com/gin-contrib/cors v1.4.0 // indirect
 	github.com/gin-gonic/gin v1.9.0 // indirect
 	github.com/go-playground/validator/v10 v10.12.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/google/s2a-go v0.1.1 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
 	github.com/leodido/go-urn v1.2.3 // indirect
 	github.com/mattn/go-isatty v0.0.18 // indirect
@@ -16,5 +20,7 @@ require (
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	golang.org/x/arch v0.3.0 // indirect
 	golang.org/x/crypto v0.8.0 // indirect
+	google.golang.org/api v0.118.0 // indirect
+	google.golang.org/genproto v0.0.0-20230410155749-daa745c078e1 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,21 +9,22 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type Employee struct {
-	Employeeid string `json:"employeeid"`
-	Firstname  string `json:"firstname"`
-	Lastname   string `json:"lastname"`
-	Divisionid string `json:"divisionid"`
-}
+// import ("encoding/json")
+// type Employee struct {
+// 	Employeeid string `json:"employeeid"`
+// 	Firstname  string `json:"firstname"`
+// 	Lastname   string `json:"lastname"`
+// 	Divisionid string `json:"divisionid"`
+// }
 
-var employees []Employee
+// var employees []Employee
 
-func getAllEmployees(w http.ResponseWriter, r *http.Request) {
-	employees = append(employees, Employee{Employeeid: "1", Firstname: "John", Lastname: "Doe", Divisionid: "1"})
-	employees = append(employees, Employee{Employeeid: "2", Firstname: "Smith", Lastname: "Taylor", Divisionid: "2"})
-	fmt.Println("Endopoint Hit: All Employees Endpoint")
-	json.NewEncoder(w).Encode(employees)
-}
+// func getAllEmployees(w http.ResponseWriter, r *http.Request) {
+// 	employees = append(employees, Employee{Employeeid: "1", Firstname: "John", Lastname: "Doe", Divisionid: "1"})
+// 	employees = append(employees, Employee{Employeeid: "2", Firstname: "Smith", Lastname: "Taylor", Divisionid: "2"})
+// 	fmt.Println("Endopoint Hit: All Employees Endpoint")
+// 	json.NewEncoder(w).Encode(employees)
+// }
 
 func main() {
 
@@ -32,6 +32,7 @@ func main() {
 
 	r := gin.Default()
 
+	// CORSはAPI Gatwayで対応済みの為、以下不要。
 	// r.Use(cors.New(cors.Config{
 	// 	AllowOrigins: []string{
 	// 		"http://localhost:3000",
@@ -58,9 +59,9 @@ func main() {
 	// }))
 
 	// r.OPTIONS("/", func(c *gin.Context) {
-	// 	c.Header("Access-Control-Expose-Headers", "*")
-	// 	c.Header("Access-Control-Allow-Origin", "*")
-	// 	c.Header("Access-Control-Allow-Credentials", "true")
+	// c.Header("Access-Control-Expose-Headers", "*")
+	// c.Header("Access-Control-Allow-Origin", "*")
+	// c.Header("Access-Control-Allow-Credentials", "true")
 	// 	c.JSON(204, gin.H{
 	// 		"message":              "hello I'm Cloud Run API 1!",
 	// 		"authorizationHeaders": c.GetHeader("Authorization"),
@@ -68,8 +69,6 @@ func main() {
 	// })
 
 	r.GET("/", func(c *gin.Context) {
-		// c.Header("Access-Control-Expose-Headers", "*")
-		// c.Header("Access-Control-Allow-Origin", "*")
 		requestDump, err := httputil.DumpRequest(c.Request, true)
 		if err != nil {
 			fmt.Println(err)
@@ -83,29 +82,4 @@ func main() {
 	if err := r.Run(":8080"); err != nil {
 		log.Fatal(err)
 	}
-
-	// コメントアウト
-	// http.HandleFunc("/", handler)
-	// http.HandleFunc("/allEmployees", getAllEmployees)
-
-	// // Determine port for HTTP service.
-	// port := os.Getenv("PORT")
-	// if port == "" {
-	// 	port = "8080"
-	// 	log.Printf("defaulting to port %s", port)
-	// }
-
-	// // Start HTTP server.
-	// log.Printf("listening on port %s", port)
-	// if err := http.ListenAndServe(":"+port, nil); err != nil {
-	// 	log.Fatal(err)
-	// }
 }
-
-// func handler(w http.ResponseWriter, r *http.Request) {
-// 	name := os.Getenv("NAME")
-// 	if name == "" {
-// 		name = "Cloud Run 1"
-// 	}
-// 	fmt.Fprintf(w, "Hello %s!\n", name)
-// }
